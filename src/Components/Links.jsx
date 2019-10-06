17dbea1d45047afd82c091decb488296f463f4f8
Heather Zurek <heather.zurek@gmail.com>
2019-10-05 22:22:14 -0700

    remove gh pages deploy

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Links(){
     return(
         <div>
-        <a href="mailto:heather.zurek@gmail.com">GMAIL</a> <span>*</span> <a href="http://www.github.com/heatherzurek">GITHUB</a> <span>*</span> <a href="http://www.twitter.com/sleepywifi">TWITTER</a> <span>*</span> <a href="http://www.linkedin.com/in/heatherzurek">LINKEDIN</a> 
+        <a href="mailto:heather.zurek@gmail.com">GMAIL </a> <a href="http://www.github.com/heatherzurek">GITHUB </a> <a href="http://www.twitter.com/sleepywifi">TWITTER </a> <a href="http://www.linkedin.com/in/heatherzurek">LINKEDIN</a> 
         <hr/>
         </div>
     );
