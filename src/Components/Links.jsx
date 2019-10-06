f8b133bed890c9c7327c047abaadc6ccd6d14a59
Heather Zurek <heather.zurek@gmail.com>
2019-10-05 23:25:39 -0700

    add font awesome icons

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Links(){
     return(
         <div>
-        <a href="mailto:heather.zurek@gmail.com">GMAIL</a> <a href="http://www.github.com/heatherzurek">GITHUB</a> <a href="http://www.twitter.com/sleepywifi">TWITTER</a> <a href="http://www.linkedin.com/in/heatherzurek">LINKEDIN</a> 
+            <a href="mailto:heather.zurek@gmail.com"><FontAwesomeIcon icon={ faEnvelope }/></a> <a href="http://www.twitter.com/sleepywifi"><FontAwesomeIcon icon={ faTwitter }/></a> <a href="http://www.github.com/heatherzurek"><FontAwesomeIcon icon={ faGithub }/></a> <a href="http://www.linkedin.com/in/heatherzurek"><FontAwesomeIcon icon={ faLinkedin }/></a>
         <hr/>
         </div>
     );
