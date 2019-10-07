f74f0d21062bc3343032a394b86e36cfcf9737e4
Heather Zurek <heather.zurek@gmail.com>
2019-10-07 09:53:34 -0700

    add to about section component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     return(
         <div>
             <h2>About</h2>
-            <p>I'm a <span class="highlight">.NET Technical Support Engineer</span> at New Relic, with an excitement for frontend software development. My pronouns are <span class="highlight">she/her</span> and I am based in <span class="highlight">Portland, Oregon</span>.</p>
+            <p>I'm a <span class="highlight">.NET Technical Support Engineer</span> at New Relic, with a background in C#, .NET, JavaScript, Node.js, and React. I work closely with .NET frameworks and SQL, but have an excitement and passion for frontend development. I enjoy learning new technologies, and there is never a shortage of things to learn. My pronouns are <span class="highlight">she/her</span>. I am based in the beautiful <span class="highlight">Pacific Northwest</span>, where I spend my summers hiking, camping, and exploring National Parks with my husband, and my spring/fall/winters trying to stay dry by staying inside and watching tv shows, programming or playing games(console or board!).</p>
         </div>
     );
 }
